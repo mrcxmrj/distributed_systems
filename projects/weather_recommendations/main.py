5829cf4d03a0de29af8001ddab522d1942a162ec
mrcxmrj <marcin.zurawel@gmail.com>
2024-03-14 20:36:09 +0100

    setup basic html

--- projects/weather_recommendations/main.py
+++ projects/weather_recommendations/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from enum import Enum
 from pydantic import BaseModel 
 from typing import List, Tuple
@@ -8,7 +9,4 @@ class Poll(BaseModel):
     options_with_votes: List[Tuple[str, int]]
 
 app = FastAPI()
-
-@app.get("/")
-async def greeting():
-    return {"message": "Hello world"}
+app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
