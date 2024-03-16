66aca79819d2e96d7c40df9633aa3d7c7f0be051
mrcxmrj <marcin.zurawel@gmail.com>
2024-03-16 18:00:19 +0100

    add auth

---
+++ projects/weather_recommendations/client/src/main.ts
@@ -0,0 +1,39 @@
+import { getAccessToken } from "./utils/getAccessToken";
+import { redirectToAuthCodeFlow } from "./utils/redirectToAuthCodeFlow";
+
+const clientId = "9bef3c4aa58f4cf781386c814edd0cc3";
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+if (!code) {
+  redirectToAuthCodeFlow(clientId);
+} else {
+  const accessToken = await getAccessToken(clientId, code);
+  const profile = await fetchProfile(accessToken);
+  console.log(profile)
+  populateUI(profile);
+}
+
+async function fetchProfile(token: string): Promise<any> {
+  const result = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET", headers: { Authorization: `Bearer ${token}` }
+  });
+
+  return await result.json();
+}
+
+function populateUI(profile: any) {
+  document.getElementById("displayName")!.innerText = profile.display_name;
+  if (profile.images[0]) {
+    const profileImage = new Image(200, 200);
+    profileImage.src = profile.images[0].url;
+    document.getElementById("avatar")!.appendChild(profileImage);
+  }
+  document.getElementById("id")!.innerText = profile.id;
+  document.getElementById("email")!.innerText = profile.email;
+  document.getElementById("uri")!.innerText = profile.uri;
+  document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
+  document.getElementById("url")!.innerText = profile.href;
+  document.getElementById("url")!.setAttribute("href", profile.href);
+  document.getElementById("imgUrl")!.innerText = profile.images[0]?.url ?? '(no profile image)';
+}
